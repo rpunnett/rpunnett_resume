bbc8f6be21beba9812a7b69984900cdbcd7829b8
popsmear <punnettr@gmail.com>
2015-01-22 20:06:46 -0500

    Adds dynamic GitHub projects

--- js/app.js
+++ js/app.js
@@ -4,15 +4,18 @@
   app.controller('ResumeController', function() {
     this.jobs = experience;
     this.education = schools;
+    this.projects = githubProjects;
     this.name = "Robert Punnett";
     this.title = "Robert Punnett Resume";
+
     this.contact = [{
-    email: 'RobertPunnett@outlook.com',
-    linkedin: "https://www.linkedin.com/in/punnettr",
-    github: "https://github.com/rpunnett",
-    stackoverflow: "https://stackoverflow.com/users/3279468/robert",
-    website: "www.RobertPunnett.com",
-  }];
+      email: 'RobertPunnett@outlook.com',
+      linkedin: "https://www.linkedin.com/in/punnettr",
+      github: "https://github.com/rpunnett",
+      stackoverflow: "https://stackoverflow.com/users/3279468/robert",
+      website: "www.RobertPunnett.com",
+    }];
+    
   });
 
 
@@ -22,42 +25,43 @@
     startDate: "March 2013",
     endDate: "Current",
     experience: [
-      "Break/fix resolution and prevention, software installs, single/mass deployments, software/hardware upgrades, virus/malware removal, hardwire/Wi-Fi network connectivity and general repair on PCs, Printers, and Mobile Devices ",
+      "Break/fix resolution and prevention, software installs, single/mass deployments, software/hardware upgrades, virus/malware resolution, network connectivity and general repair on PCs, Printers, and Mobile Devices ",
       "Image, configure, upgrade and deploy computers using Windows MDT and Ghost",
       "Assisted in large scale roll out of Windows 7 with attention to detail in client requirements and satisfaction",
-      "Assist other teams as needed in projects such as Wake-on-LAN, large scale software deployments and GPO",
-      "Designed, coded and deployed an asset management system using a web-based front-end and a PowerShell/C# back-end to automate the collection and increase the manageability of asset information for individual teams, vastly increasing maintenance and reporting capability for managers and bringing together separate divisions to unify processes",
-      "Wrote a large repository of scripts using Batch, Visual Basic and PowerShell designed to automate common and complex tasks, such as automated one-click installs, issue prevention and data migration",
-      "Created internal documentation on software, hardware and processes on a team and department level",
-      "Researched and led a project for a department wide knowledge-base to streamline the process of retaining, creating and accessing relevant information "
+      "Assist other teams or departments as needed in projects such as Wake-on-LAN management and GPO adjustments",
+      "Designed, coded and deployed a asset management system using a web-based front-end and a PowerShell/C# back-end as a full stack developer",
+      "Created a RESTful API to interface with the asset management system",
+      "Researched and led a project for a department wide knowledge-base",
+      "Wrote a large repository of scripts using Batch, Visual Basic and PowerShell ",
+      "Created internal documentation on software, hardware and processes on a team and department level"
     ]
   }, {
-   	companyName: 'Wandering WiFi',
+    companyName: 'Wandering WiFi',
     title: "Telecommunications Manager",
     startDate: "January 2012",
     endDate: "March 2013",
     experience: [
-      "Led team that procured and managed telecom accounts totaling $4 Million annually ranging from DSL, Cable, T1, WISP, WiMAX and Cellular providers ",
+      "Led team that procured and managed telecom accounts totaling $4 Million annually across DSL, Cable, T1, WISP, WiMAX and Cellular providers ",
       "Created comprehensive process documentation ",
-      "Designed an internal process to create metrics for growth and streamlined the audit process to eliminate excess overhead and lost income",
-      "Renegotiated multiple contracts by analyzing cost structures across geographies, saving 20% on average",
+      "Designed internal metrics and streamlined processes to eliminate lost income",
+      "Renegotiated contracts, saving 20% on average",
     ]
   }, {
-   	companyName: 'Wandering WiFi',
+    companyName: 'Wandering WiFi',
     title: "Technical Account Manager",
     startDate: "September 2008",
     endDate: "March 2013",
     experience: [
       "Managed and trained a rotating team of five to oversee 1,000+ locations with an annual revenue of $7 Million and a monthly user base of 540,000",
-      "Conducted three initial rollouts totaling $2.5 Million in capital over 600 locations that involved the installation of WLAN/LAN networks, marketing design and managing third party resources",
-      "Led a $215,000 capital rollout to migrate to a Cisco T1 telecom solution at 220 locations ",
-      "Cultivated corporate client relationships by continually meeting and exceeding expectations ",
+      "Conducted three new client rollouts totaling $2.5 Million in capital over 600 locations that involved the installation of WLAN/LAN networks, marketing design and managing third party resources",
+      "Led a migration project for a Cisco T1 telecom solution at 220 locations ",
+      "Cultivated corporate client relationships by continually meeting and exceeding expectations",
       "Engineered initiatives that restructured technical support roles, training, hiring processes and client communication",
       "Wrote technical and support documentation on hardware, troubleshooting and process improvement",
-      "Coordinated DNS content filtering for external clients by moving from an internal to an external solution reducing customer complaints by 90%",
+      "Researched and led a project to migrate DNS content filtering from an internal server to an external solution; reducing customer complaints by 90%",
     ]
   }, {
-   	companyName: 'Wandering WiFi',
+    companyName: 'Wandering WiFi',
     title: "Technical Support",
     startDate: "March 2008",
     endDate: "Septermber 2008",
@@ -69,7 +73,7 @@
       "Consistently awarded for performance",
     ]
   }, {
-   	companyName: 'Innovative Computers',
+    companyName: 'Innovative Computers',
     title: "IT Analyst",
     startDate: "June 2006",
     endDate: "January 2008",
@@ -94,22 +98,32 @@
 
 
 
-  var githubProjects = [
-  {
+  var githubProjects = [{
+    title: 'rpunnett_resume',
+    description: "This Resume, built using AngularJS and Sass",
+    technology: ["JavaScript","CSS"],
+    url: "https://github.com/rpunnett/rpunnett_resume",
+  }, {
     title: 'extract_imgur',
     description: " A small class to extract image links from imgur URLS in PHP",
     technology: ["PHP"],
     url: "https://github.com/rpunnett/extract_imgur",
-  },
-    {
+  }, {
     title: 'basic_linux_kernel',
     description: "A small linux kernel that prints a string to console.",
     technology: ["Assembly","C"],
     url: "https://github.com/rpunnett/basic_linux_kernel",
-  }
+  }];
 
-  ];
+app.directive('href', function() {
+  return {
+    compile: function(element) {
+      element.attr('target', '_blank');
+    }
+  };
+});
 
 })();
+
 
 
